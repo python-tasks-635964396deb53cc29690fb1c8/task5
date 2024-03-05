5b56c22174a01aa49be82ee64d62f738a4f6fa73
jEUGENEdev <genyamateyuk@gmail.com>
2024-03-06 00:03:28 +0300

    function  get_ipv4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ipaddress import IPv4Address
 from re import match, I
 
 pattern = r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$'
@@ -5,3 +6,10 @@ pattern = r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$'
 
 def is_ipv4(address: str) -> bool:
     return bool(match(pattern, address, I))
+
+
+def get_ipv4(address: str) -> IPv4Address:
+    if not is_ipv4(address):
+        raise RuntimeError()
+
+    return IPv4Address(address)
